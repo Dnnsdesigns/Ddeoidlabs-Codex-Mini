141936999c77cd6a9eb5019826ffce1f0ddb51ce
Dennis Duncan <144344247+Dnnsdesigns@users.noreply.github.com>
2024-07-14 21:13:45 -0700

    Create project-mgr-agent.py

---
+++ project-mgr-agent.py
@@ -0,0 +1,10 @@
+# project_management_agent.py
+class ProjectManagementAgent:
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def process(self, task_data):
+        inputs = self.tokenizer(task_data['task'], return_tensors='pt')
+        outputs = self.model.generate(**inputs)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
